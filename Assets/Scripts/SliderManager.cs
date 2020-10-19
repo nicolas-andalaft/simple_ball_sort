9e3dba66b366bd23270845440351668987411f09
Nicolas Andalaft <nicolas.andalaft@hotmail.com>
2020-10-19 03:57:15 -0300

    Level customizer dialog

---
+++ Assets/Scripts/SliderManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderManager : MonoBehaviour
+{
+    [SerializeField] private Text indicator;
+
+    public void updateIndicator(float single)
+    {
+        indicator.text = single.ToString();
+    }
+}
